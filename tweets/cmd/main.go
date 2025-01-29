48595b1944c3d4bdbc9353a45df08f8e42ce8ea1
Stevenstr <stephenzhavrid@gmail.com>
2025-01-29 17:23:50 +0300

    repository for tweet was implemented

--- tweets/cmd/main.go
+++ tweets/cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"tweets.com/tweets/internal/repository/memory"
+)
+
 func main() {
+
+	repo := memory.New()
 
 }
--- tweets/internal/repository/memory/memory.go
+++ tweets/internal/repository/memory/memory.go
@@ -1,1 +1,39 @@
 package memory
+
+import (
+	"context"
+	"sync"
+
+	"tweets.com/tweets/internal/repository"
+	"tweets.com/tweets/pkg/model"
+)
+
+type Repository struct {
+	sync.RWMutex
+	data map[string]*model.Tweets
+}
+
+func New() *Repository {
+	return &Repository{data: map[string]*model.Tweets{}}
+}
+
+func (r *Repository) Get(_ context.Context, id string) (*model.Tweets, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	m, ok := r.data[id]
+	if !ok {
+		return nil, repository.ErrNotFound
+	}
+
+	return m, nil
+}
+
+func (r *Repository) Put(_ context.Context, id string, tweet *model.Tweets) error {
+	r.RLock()
+	defer r.Unlock()
+
+	r.data[id] = tweet
+
+	return nil
+}
